a1687f779d0c0958ae44c0d3ea1cedee7a531020
meyerbe <meyerbe@ethz.ch>
2018-07-31 15:42:42 +0200

    adding 4-hourly rr histograms incl. percentiles

--- Python_Code/PDF_plotting_functions.py
+++ Python_Code/PDF_plotting_functions.py
@@ -5,42 +5,63 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
 
-def plot_scatter_all(mean, variance, meanmean, varmean, extremes, dates):
+def plot_scatter_daily(mean, variance, max, meanmean, varmean, extremes, dates):
 
     print('plotting')
 
     # Now switch to a more OO interface to exercise more features.
     # fig, axs = plt.subplots(nrows=1, ncols=2, sharex=True, figsize=(20,9))
-    fig, axs = plt.subplots(nrows=1, ncols=2, sharex=False, figsize=(20,9))
-    ax = axs[0] # ax = axs[0, 0]
-    ax.plot(dates, mean, '.')
+    plt.figure(figsize=(30,9))
+    # fig, axs = plt.subplots(nrows=1, ncols=2, sharex=False, figsize=(30,9))
+    plt.subplot(1,3,1)
+    plt.plot(dates, mean, '.')
     for i in range(len(extremes)):
-        ax.plot(extremes[i][2], extremes[i][1], '.r')
-    ax.plot([dates[0],dates[-1]], [meanmean,meanmean], 'k--', linewidth=1, label='mean(means)')
-    ax.plot([dates[0],dates[-1]], [meanmean+varmean,meanmean+varmean], 'k', linewidth=1, label=r'mean(means)$\pm$2*var(means)')
-    ax.plot([dates[0],dates[-1]], [meanmean-varmean,meanmean-varmean], 'k', linewidth=1)
-    ax.set_title('mean rain rates')
-    ax.legend()
+        plt.plot(extremes[i][2], extremes[i][1], '.r')
+    plt.plot([dates[0],dates[-1]], [meanmean,meanmean], 'k--', linewidth=1, label='mean(means)')
+    plt.plot([dates[0],dates[-1]], [meanmean+varmean,meanmean+varmean], 'k', linewidth=1, label=r'mean(means)$\pm$var(means)')
+    plt.plot([dates[0],dates[-1]], [meanmean-varmean,meanmean-varmean], 'k', linewidth=1)
+    plt.plot([dates[0],dates[-1]], [meanmean+2*varmean,meanmean+2*varmean], '0.75', linewidth=1, label=r'mean(means)$\pm$2*var(means)')
+    plt.plot([dates[0],dates[-1]], [meanmean-2*varmean,meanmean-2*varmean], '0.75', linewidth=1)
+    plt.title('mean rain rates')
+    plt.legend()
     plt.xlabel('dates')
-    plt.ylabel('mean')
+    plt.ylabel('mean (rain rate [mm/h])')
 
-    ax = axs[1] #ax = axs[0, 1]
-    ax.errorbar(dates, mean, yerr=variance, fmt='o')#, color='b')
+    plt.subplot(1,3,2)
+    plt.errorbar(dates, mean, yerr=variance, fmt='o')#, color='b')
     # ax.errorbar(extremes[:][2], extremes[:][1], yerr=variance, fmt='o', color='r')
     # ax.errorbar(extremes[:][2], extremes[:][1], fmt='o', color='r')
     for i in range(len(extremes)):
-        ax.errorbar(extremes[i][2], extremes[i][1], fmt='o', color='r')
-    ax.plot([dates[0], dates[-1]], [meanmean + varmean, meanmean + varmean], 'k', linewidth=1)
-    ax.plot([dates[0], dates[-1]], [meanmean - varmean, meanmean - varmean], 'k', linewidth=1)
-    ax.set_title('mean + var')
+        plt.errorbar(extremes[i][2], extremes[i][1], fmt='o', color='r')
+    plt.plot([dates[0], dates[-1]], [meanmean + varmean, meanmean + varmean], 'k', linewidth=1)
+    plt.plot([dates[0], dates[-1]], [meanmean - varmean, meanmean - varmean], 'k', linewidth=1)
+    plt.title('mean + var')
     plt.xlabel('dates')
-    plt.ylabel('mean')
+    plt.ylabel('mean (rain rate [mm/h])')
+    plt.savefig('./alldata_mean_var.png')
+
+    plt.subplot(1,3,3)
+    plt.errorbar(dates, mean, yerr=variance, fmt='o', label=r'daily mean$\pm$var')#, color='b')
+    for i in range(len(extremes)):
+        if i == 0:
+            plt.errorbar(extremes[i][2], extremes[i][1], fmt='o', color='r', label='extremes')
+        else:
+            plt.errorbar(extremes[i][2], extremes[i][1], fmt='o', color='r')
+    plt.plot(dates, max, '.-', label='daily max values')  # , '.-', '0.4', label='daily max values')
+    plt.plot([dates[0], dates[-1]], [meanmean + varmean, meanmean + varmean], 'k', linewidth=1)
+    plt.plot([dates[0], dates[-1]], [meanmean - varmean, meanmean - varmean], 'k', linewidth=1)
+    plt.legend(loc='best')
+    plt.title('mean + var')
+    plt.xlabel('dates')
+    plt.ylabel('mean (rain rate [mm/h])')
+
+    plt.suptitle('Daily mean rain rates', fontsize=21)
     plt.savefig('./alldata_mean_var.png')
     return
 
 
 
-def plot_histogram_all(mean, variance, meanmean, varmean, extremes, date_list):
+def plot_histogram_daily(mean, variance, meanmean, varmean, extremes, date_list):
 
     import matplotlib.mlab as mlab
 
@@ -49,12 +70,11 @@ def plot_histogram_all(mean, variance, meanmean, varmean, extremes, date_list):
 
     plt.figure(figsize=(20,9))
     plt.subplot(1,2,1)
-    # plt.hist(mean)
     num_bins = 20
     n, bins, patches = plt.hist(mean, num_bins, density=False, facecolor='green', alpha=0.5)
-    plt.xlabel('mean')
+    plt.xlabel('mean (rain rate [mm/h])')
     plt.ylabel('p(mean)')
-    plt.title('all date, unnormed')
+    plt.title('all dates, unnormed')
 
     plt.subplot(1,2,2)
     num_bins = 50
@@ -62,10 +82,12 @@ def plot_histogram_all(mean, variance, meanmean, varmean, extremes, date_list):
     y = scipy.stats.norm.pdf(bins, meanmean, varmean)
     plt.plot(bins, y, 'r--', label='best fit normal distribution')
     plt.legend(loc='best')
-    plt.xlabel('mean')
+    plt.xlabel('mean (rain rate [mm/h])')
     plt.ylabel('p(mean)')
-    plt.title('all date, normed')
-    plt.savefig('./alldata_hist.png')
+    plt.title('all dates, normed')
+
+    plt.suptitle('Histogram daily mean rain rates', fontsize=21)
+    plt.savefig('./alldata_hist_means.png')
 
     # plt.subplot(1, 3, 3)
     # # add a 'best fit' line
@@ -80,5 +102,129 @@ def plot_histogram_all(mean, variance, meanmean, varmean, extremes, date_list):
     # y = scipy.stats.norm.pdf(bins, mu, sigma)
     # plt.plot(bins, y, 'r--', label='best fit normal distribution')
 
+
+    return
+
+
+
+def plot_histogram_4hourly(hist, bin_arr, bin_max, per_arr, date_list, n_files, n_time, lat, long):
+    cm = plt.cm.get_cmap('gist_rainbow')
+    cm_r = plt.cm.get_cmap('gist_rainbow_r')
+    n_tot = per_arr.shape[1]
+
+    plt.figure(figsize=(27, 9))
+    plt.subplot(1, 3, 1)
+    width = 1. * (bin_arr[1] - bin_arr[0])
+    center = (bin_arr[:-1] + bin_arr[1:]) / 2
+    plt.bar(center, hist, align='center', width=width)
+    plt.title('(bin-width=0.1)')
+    # width = 0.75 * (bin_arr_large[1] - bin_arr_large[0])
+    # center = (bin_arr_large[:-1] + bin_arr_large[1:]) / 2
+    # plt.bar(center, hist_l, align='center', width=width)
+    # plt.title('(bin-width=1.0)')
+    plt.ylim([0, hist[2] * 1.001])
+    plt.xlabel('rain rate [mm/h]   (4-hourly mean)')
+    plt.ylabel('p(rr)')
+
+    plt.subplot(1, 3, 2)
+    width = .75 * (bin_arr[1] - bin_arr[0])
+    center = (bin_arr[:bin_max] + bin_arr[1:(bin_max + 1)]) / 2
+    plt.bar(center, hist[:bin_max], align='center', width=width)
+    plt.title('(bin-width=0.1)')
+    # width = 0.75 * (bin_arr_large[1] - bin_arr_large[0])
+    # center = (bin_arr_large[:-1] + bin_arr_large[1:]) / 2
+    # plt.bar(center, hist_l, align='center', width=width)
+    # plt.title('(bin-width=1.0)')
+    # percentiles:
+    for i, ip in enumerate(per_arr[2,:]):
+        ip = np.int(ip)
+        if ip < bin_max:
+            plt.bar(center[ip], hist[ip], align='center', width=width,
+                # color = 'r', #
+                color=cm_r(np.double(i)/n_tot),
+                label=np.str(per_arr[0,i])+'th percentile (i='+np.str(ip)+')')
+    plt.legend(loc='best')
+    y_max = n_files * n_time * lat * long * 0.01
+    plt.ylim([0, y_max])
+    plt.xlabel('rain rate [mm/h]   (4-hourly mean)')
+    plt.ylabel('p(rr)       (max: 0.01 * n_points)')
+
+    plt.subplot(1, 3, 3)
+    bin_max_ = 30
+    width = 0.75 * (bin_arr[1] - bin_arr[0])
+    center = (bin_arr[:bin_max_] + bin_arr[1:(bin_max_ + 1)]) / 2
+    plt.bar(center, hist[:bin_max_], align='center', width=width)
+    # percentiles:
+    for i, ip in enumerate(per_arr[2, :]):
+        ip = np.int(ip)
+        if ip < bin_max_:
+            plt.bar(center[ip], hist[ip], align='center', width=width,
+                color=cm_r(np.double(i)/n_tot),
+                label=np.str(per_arr[0, i]) + 'th percentile')
+    plt.legend(loc='best')
+    plt.ylim([0, hist[1] * 1.01])
+    plt.xlabel('rain rate [mm/h]   (4-hourly mean)')
+    plt.ylabel('p(rr)       (max: hist[1])')
+    plt.title('(bin-width=0.1)')
+
+    plt.suptitle('Histogram: 4-hourly rain rates ('
+                 +np.str(date_list[0]) + '-' + np.str(date_list[-1]) + ')', fontsize=21)
+    plt.savefig('./hist_all.png')
+    plt.close()
+
+
+
+
+    # Histogram wrt hist[1:]
+    plt.figure(figsize=(27, 9))
+    plt.subplot(1, 3, 1)
+    width = 1. * (bin_arr[1] - bin_arr[0])
+    center = (bin_arr[:-1] + bin_arr[1:]) / 2
+    plt.bar(center, hist, align='center', width=width)
+    plt.title('(bin-width=0.1)')
+    plt.ylim([0, hist[2] * 1.001])
+    plt.xlabel('rain rate [mm/h]   (4-hourly mean)')
+    plt.ylabel('p(rr)')
+
+    plt.subplot(1, 3, 2)
+    width = .75 * (bin_arr[1] - bin_arr[0])
+    center = (bin_arr[:bin_max] + bin_arr[1:(bin_max + 1)]) / 2
+    plt.bar(center, hist[:bin_max], align='center', width=width)
+    plt.title('(bin-width=0.1)')
+    # percentiles:
+    for i, ip in enumerate(per_arr[3,:]):
+        ip = np.int(ip)
+        if ip < bin_max:
+            plt.bar(center[ip], hist[ip], align='center', width=width,
+                color=cm(np.double(i)/n_tot), hatch='//',
+                label=np.str(per_arr[0,i])+'th percentile (i='+np.str(ip)+')')
+    plt.legend(loc='best')
+    y_max = n_files * n_time * lat * long * 0.01
+    plt.ylim([0, y_max])
+    plt.xlabel('rain rate [mm/h]   (4-hourly mean)')
+    plt.ylabel('p(rr)       (max: 0.01 * n_points)')
+
+    plt.subplot(1, 3, 3)
+    bin_max = 30
+    width = 0.75 * (bin_arr[1] - bin_arr[0])
+    center = (bin_arr[:bin_max] + bin_arr[1:(bin_max + 1)]) / 2
+    plt.bar(center, hist[:bin_max], align='center', width=width)
+    # percentiles:
+    for i, ip in enumerate(per_arr[3, :]):
+        ip = np.int(ip)
+        if ip < bin_max:
+            plt.bar(center[ip], hist[ip], align='center', width=width,
+                color=cm(np.double(i) / n_tot), hatch='//',
+                label=np.str(per_arr[0, i]) + 'th percentile')
+    plt.legend(loc='best')
+    plt.ylim([0,hist[1]*1.01])
+    plt.xlabel('rain rate [mm/h]   (4-hourly mean)')
+    plt.ylabel('p(rr)       (max: hist[1])')
+    plt.title('(bin-width=0.1)')
+
+    plt.suptitle('Histogram: 4-hourly rain rates; Percentiles wrt Hist[1:] ('
+                 + np.str(date_list[0]) + '-' + np.str(date_list[-1]) + ')', fontsize=21)
+    plt.savefig('./hist_all_perc2.png')
+    plt.close()
 
     return
--- Python_Code/PDF_rain_intensity.py
+++ Python_Code/PDF_rain_intensity.py
@@ -2,13 +2,14 @@ import os, sys
 # import argparse
 # import json as simplejson
 import numpy as np
-# import pylab as plt
 import netCDF4 as nc
+import matplotlib.pyplot as plt
 # from matplotlib.colors import LogNorm
 import glob
 import ntpath
 
-from PDF_plotting_functions import plot_scatter_all, plot_histogram_all
+from PDF_plotting_functions import plot_scatter_daily, plot_histogram_daily
+from PDF_plotting_functions import plot_histogram_4hourly
 
 def main():
     print('main')
@@ -16,17 +17,9 @@ def main():
     # path_data = '/Users/bettinameyer/Dropbox/ClimatePhysics/Code/Tracking/RadarData_Darwin/RADAR_ESTIMATED_RAIN_RATE'
     path_data = '/Users/bettinameyer/Dropbox/ClimatePhysics/Code/Tracking/RadarData_Darwin/RADAR_ESTIMATED_RAIN_RATE_test'
     files = [name for name in os.listdir(path_data) if name[-2:]=='nc']
-    # print(files)
-    # print(type(files))
     n_files = len([name for name in os.listdir(path_data) if name[-2:]=='nc'])
     print('# files: ' + np.str(n_files))
     print('')
-
-    # y = 2002; m = 1; d = 1
-    # date = np.str(y*10000 + m*100 + d)
-    # file_name = 'CPOL_RADAR_ESTIMATED_RAIN_RATE_' + date + '.nc'
-    # path_in = os.path.join(path_data, file_name)
-    # # var_name = 'radar_estimated_rain_rate'
 
     ''' Read in test file '''
     path_in = os.path.join(path_data, files[0])
@@ -42,9 +35,16 @@ def main():
     # for (2)
     mean = np.zeros(shape=(n_files))
     variance = np.zeros(shape=(n_files))
+    max = np.zeros(shape=(n_files))
     date_list = []
 
     index = 0
+
+    # num_bins = 200
+    bin_arr = np.append(np.append(np.arange(0,10,0.1),np.arange(10,100,1)), np.arange(100,201,10))
+    bin_arr = np.append(np.append(np.arange(0,50,0.1),np.arange(50,100,1)), np.arange(100,201,10))
+    bin_arr_large = np.arange(0,201,1)
+    bin_arr_test = np.arange(0,110,10)
     for path_in in glob.glob(os.path.join(path_data, '*')):
         data_name = ntpath.basename(path_in)[:-3]
         date = data_name[-8:]
@@ -61,50 +61,119 @@ def main():
         data_rr = np.ndarray(shape=var.shape)
         data_rr = var[:]
         rootgrp.close()
-
-        # # (1) PDF from all data
-        # aux = np.zeros(shape=(data1.shape[0] - data2.shape[0], nz))
-        # data_all = np.append(data_all, aux, axis=0)
-        # print('shapes', data1.shape, data2.shape)
-        # print('')
-        # del aux
-
-        # (2) PDF for each day
         data = np.ravel(data_rr)
-        # print(data.size, data.shape)
-        # print(data_rr.size, data_rr.shape)
+
+        ''' (1) Histogram from 4-hourly data '''
+        if index == 0:
+            hist, bin_edges = np.histogram(data, bins=bin_arr, normed=False)
+            hist_test, bin_edges_test = np.histogram(data, bins=bin_arr_test, normed=False)
+            # hist_l, bin_edges_l = np.histogram(data, bins=bin_arr_large, normed=False)
+        else:
+            hist += np.histogram(data, bins=bin_arr, normed=False)[0]
+            hist_test += np.histogram(data, bins=bin_arr_test, normed=False)[0]
+            # hist_l += np.histogram(data, bins=bin_arr_large, normed=False)[0]
+
+        ''' (2) Statistics / Histogram of daily mean '''
         mean[index] = np.mean(data)
         variance[index] = np.var(data)
+        max[index] = np.amax(data)
 
 
         index += 1
 
+
+    # ???? for 2001-2003: hist[0]<hist[1], hist[2]<hist[1] ???? why not first component the largest???
+
+    ''' (1) plotting 4-hourly mean '''
+    # hist:         array with number of points that fall in the bins defined by bin_arr (np.shape(hist) = np.shape(bin_arr))
+    # hist[0] ~ 95.7% of cumulated points (np.sum(hist[:]))
+    # hist[1] ~ 0.84% of cumulated points (np.sum(hist[:])); 19.4% of cumulated points from 1 (np.sum(hist[1:]))
+    # hist[2] ~ 0.5% of cumulated points (np.sum(hist[:]))
+
+    # hist_test:
+    # bin_arr_test = np.arange(0,110,10) = [0, 10, ..., 100]
+    # hist_test:     array of length 10 with 99.6% of points in first bin (0 < rr < 10)
+    # print('')
+    # print('bin arr test', bin_arr_test)
+
+    print('')
+    print('hist:', hist.shape, type(hist), hist[0], hist[1], hist[2])
+    print('hist[0]:', np.double(hist[0])/np.sum(hist))
+    print('hist[1]:', np.double(hist[1])/np.sum(hist), np.double(hist[1])/np.sum(hist[1:]))
+    print('hist[2]:', np.double(hist[2])/np.sum(hist), np.double(hist[2])/np.sum(hist[1:]))
+    print('')
+
+    ''' (1a) computing percentiles '''
+    # arr_per:        array with percentiles and the index of the correspondind bins (incl. / excl. first bin)
+    # arr_per[0]:     percentiles
+    # arr_per[1]:     number of points in that percentile
+    # arr_per[2]:     index of bin into which the percentile falls (incl. the first bin)
+    # arr_per[3]:     index of bin into which the percentile falls (excl. the first bin)
+    per_arr = np.asarray([10,20,50,75,90,95,96,97,98,99,99.9], dtype=np.double)
+    n_per = per_arr.shape[0]
+    per_arr = np.append(per_arr, 0.01*np.sum(hist)*per_arr).reshape(2,n_per)
+    per_arr = np.append(per_arr, np.zeros(shape=per_arr.shape[1])).reshape(3,n_per)
+    per_arr = np.append(per_arr, np.zeros(shape=per_arr.shape[1])).reshape(4,n_per)
+
+    print('cum: ', np.sum(hist), 'hist[0]:', hist[0], 'hist[1]:', hist[1])
+    for ip, per in enumerate(per_arr[1,:]):
+        # per = np.percentile(hist, p)
+        print(
+        'percentile ' + np.str(per_arr[0, ip]) + '%: ' + np.str(np.int(per)))
+        i = 0
+        cum = hist[i]
+        while (cum < per):
+
+            i += 1
+            cum += hist[i]
+            # print('cum', cum, 'percentile', per, 'i', i)
+        print('cum[-1]=' + np.str(np.sum(hist[:i])), 'cum=' + np.str(np.sum(hist[:i + 1])), 'i=' + np.str(i))
+        per_arr[2,ip] = i
+    print('')
+
+    per_arr_ = 0.01 * np.sum(hist[1:]) * per_arr[0,:]
+    for ip, per in enumerate(per_arr_):
+        # per_1 = np.percentile(hist[1:], p)
+        i = 1
+        cum = hist[1]
+        while (cum < per):
+            i += 1
+            cum += hist[i]
+        per_arr[3,ip] = i
+        print('_percentile ' + np.str(per_arr[0,ip]) + '%: ' + np.str(np.int(per)), 'cum=' + np.str(np.sum(hist[:i + 1])),
+              'i=' + np.str(i))
+
+
+    bin_max = 100
+    plot_histogram_4hourly(hist, bin_arr, bin_max, per_arr, date_list, n_files, n_time, lat, long)
+
+
+
+    ''' (2) plotting daily mean '''
     meanmean = np.mean(mean)
     varmean = np.var(mean)
     extremes = []
     for i,f in enumerate(files):
         if mean[i] > (meanmean+2*varmean) or mean[i] < (meanmean-2*varmean):
             extremes.append([i, mean[i], f[-11:-3]])
-
-    plot_scatter_all(mean, variance, meanmean, varmean, extremes, date_list)
-
-    plot_histogram_all(mean, variance, meanmean, varmean, extremes, date_list)
+    plot_scatter_daily(mean, variance, max, meanmean, varmean, extremes, date_list)
+    plot_histogram_daily(mean, variance, meanmean, varmean, extremes, date_list)
 
 
     return
 
 
-# ----------------------------------------------------------------------
-def read_in_netcdf(variable_name, fullpath_in):
-    rootgrp = nc.Dataset(fullpath_in, 'r')
-    var = rootgrp.groups['fields'].variables[variable_name]
-
-    shape = var.shape
-    print('shape:', var.shape)
-    data = np.ndarray(shape=var.shape)
-    data = var[:]
-    rootgrp.close()
-    return data
+# # ----------------------------------------------------------------------
+# def read_in_netcdf(variable_name, fullpath_in):
+#     rootgrp = nc.Dataset(fullpath_in, 'r')
+#     var = rootgrp.groups['fields'].variables[variable_name]
+#
+#     shape = var.shape
+#     print('shape:', var.shape)
+#     data = np.ndarray(shape=var.shape)
+#     data = var[:]
+#     rootgrp.close()
+#     return data
 
 # ----------------------------------------------------------------------
 if __name__ == "__main__":
